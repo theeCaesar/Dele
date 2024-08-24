2b92f98b696e2bddb74ef863f0033538a99a45c6
theeCaesar <workthecaesar@gmail.com>
2024-08-25 01:32:03 +0300

    commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,7 @@
 import Users from "../models/users.js";
 
 const deleteUser = async (req, res) => {
-  const { phoneNumber } = req.params.num;
+  const { phoneNumber } = req.query.num;
   try {
     const user = await Users.findOneAndDelete({ phoneNumber });
     res.status(200).json({ message: "user has been deleted" });
